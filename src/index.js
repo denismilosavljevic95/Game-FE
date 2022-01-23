1e2fece02c16fdddf7492a528d49f27810b5436a
Denis <denismilosavljevic1995@gmail.com>
2022-01-23 15:42:43 +0100

    New init 	new file:   .eslintrc.json 	modified:   README.md 	deleted:    _babelrc 	modified:   package-lock.json 	modified:   package.json 	modified:   public/favicon.ico 	modified:   public/index.html 	modified:   src/App.js 	modified:   src/App.test.js 	deleted:    src/history.js 	modified:   src/index.css 	modified:   src/index.js 	modified:   src/logo.svg 	new file:   src/reportWebVitals.js 	deleted:    src/serviceWorker.js 	modified:   src/setupTests.js 	deleted:    src/store/actionTypes.js 	deleted:    src/store/actions.js 	deleted:    src/store/reducer.js 	deleted:    src/store/sagas/index.js 	deleted:    src/translationCatalog.json 	deleted:    src/utils/utils.js 	deleted:    webpack.config.js 	deleted:    yarn.lock

--- src/index.js
+++ src/index.js
@@ -2,27 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { Router } from "react-router-dom";
-import { createStore, applyMiddleware } from 'redux'
-import reducer from './store/reducer';
-import { Provider } from 'react-redux'
-import createSagaMiddleware from 'redux-saga';
-import { watchPosts } from './store/sagas';
-import history from './history'
+import reportWebVitals from './reportWebVitals';
 
-const sagaMiddleware = createSagaMiddleware();
-
-const store = createStore(
-  reducer,
-  applyMiddleware(sagaMiddleware),
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-sagaMiddleware.run(watchPosts);
-
-ReactDOM.render(<Provider store={store}><Router history={ history }><App /></Router></Provider>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- src/store/sagas/index.js
+++
@@ -1,3 +0,0 @@
-import { takeEvery } from "redux-saga/effects";
-
-import * as actionTypes from "../actionTypes";
